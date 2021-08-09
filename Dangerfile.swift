8372c2b4fe23bec0430ac94e34cde0f0a1f88bf5
Joseph Mattiello <mail@joemattiello.com>
2021-08-09 03:59:17 -0400

    Dangerfile updates

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -1,70 +1,93 @@
-# PR is a work in progress and shouldn't be merged yet
-warn "PR is classed as Work in Progress" if github.pr_title.include? "[WIP]"
+import Danger
+import Foundation
+// import SwiftLint
 
-# Warn when there is a big PR
-warn "Big PR, consider splitting into smaller" if git.lines_of_code > 600
+let danger = Danger()
 
-# Ensure a clean commits history
-if git.commits.any? { |c| c.message =~ /^Merge branch '#{github.branch_for_base}'/ }
-  fail "Please rebase to get rid of the merge commits in this PR"
-end
+// fileImport: DangerfileExtensions/ChangelogCheck.swift
+checkChangelog()
 
-# Mainly to encourage writing up some reasoning about the PR, rather than
-# just leaving a title
-if github.pr_body.length < 5
-  fail "Please provide a summary in the Pull Request description"
-end
+if danger.git.createdFiles.count + danger.git.modifiedFiles.count - danger.git.deletedFiles.count > 600 {
+    warn("Big PR, try to keep changes smaller if you can")
+}
 
-# If these are all empty something has gone wrong, better to raise it in a comment
-if git.modified_files.empty? && git.added_files.empty? && git.deleted_files.empty?
-  fail "This PR has no changes at all, this is likely an issue during development."
-end
+let swiftFilesWithCopyright = danger.git.createdFiles.filter {
+    $0.fileType == .swift
+        && danger.utils.readFile($0).contains("//  Created by")
+}
 
-# has_app_changes = !git.modified_files.grep(/Provenance$/).empty?
-# has_app_test_changes = !git.modified_files.grep(/Provenance/).empty?
-#
-# has_support_changes = !git.modified_files.grep(/PVSupport/).empty?
-# has_support_test_changes = !git.modified_files.grep(/PVSupport Tests/).empty?
-#
-# has_library_changes = !git.modified_files.grep(/PVLibrary/).empty?
-# has_library_test_changes = !git.modified_files.grep(/PVLibrary Tests/).empty?
-#
-# # If changes are more than 10 lines of code, tests need to be updated too
-# if ((has_core_changes && !has_core_test_changes) || (has_coreui_changes && !has_coreui_test_changes)) && git.lines_of_code > 10
-#   fail("Tests were not updated", sticky: false)
-# end
+if !swiftFilesWithCopyright.isEmpty {
+    let files = swiftFilesWithCopyright.joined(separator: ", ")
+    warn("In Danger JS we don't include copyright headers, found them in: \(files)")
+}
 
-# Info.plist file shouldn't change often. Leave warning if it changes.
-is_plist_change = git.modified_files.grep[/Info.plist/].empty?
+// # This is swiftlint plugin. More info: https://github.com/ashfurrow/danger-swiftlint
+// #
+// # This lints all Swift files and leave comments in PR if 
+// # there is any issue with linting
+let filesToLint = (danger.git.modifiedFiles + danger.git.createdFiles) // .filter { !$0.contains("Documentation/") }
+
+SwiftLint.lint(.files(filesToLint), inline: true)
+
+// Support running via `danger local`
+if danger.github != nil {
+    // These checks only happen on a PR
+    if danger.github.pullRequest.title.contains("WIP") {
+        warn("PR is classed as Work in Progress")
+    }
+}
+
+if github.pr_title.contains("WIP") {
+    warn("PR is classed as Work in Progress")
+}
+
+if git.commits.any {
+  return $0.message.contains("Merge branch '\(github.branch_for_base)'")
+} {
+  fail("Please rebase to get rid of the merge commits in this PR ")
+}
+
+if github.pr_body.length > 1000 {
+    warn("PR body is too long")
+}
+
+if github.pr_body.length < 5 {
+  fail("PR body is too short")
+}
+
+
+let has_app_changes = !git.modified_files.any { $0.contains("Provenance") }
+let has_support_changes = !git.modified_files.any { $0.contains("PVSupport") }
+let has_support_test_changes = !git.modified_files.any { $0.contains("PVSupport Tests") }
+let has_library_changes = !git.modified_files.any { $0.contains("PVLibrary") }
+let has_library_test_changes = !git.modified_files.any { $0.contains("PVLibrary Tests") }
+
+// If changes are more than 10 lines of code, tests need to be updated too
+// if (has_core_changes && !has_core_test_changes) ||
+//     (has_coreui_changes && !has_coreui_test_changes)) &&
+//     git.lines_of_code > 10 {
+//   fail("Tests were not updated", sticky: false)
+// }
+
+// Info.plist file shouldn't change often. Leave warning if it changes.
+let is_plist_change = git.modified_files.any { $0.contains("Info.plist") }
 
 if !is_plist_change
-  warn "A Plist was changed"
-  # warn "Plist changed, don't forget to localize your plist values"
+  // warn "A Plist was changed"
+  warn("Plist changed, don't forget to localize your plist values")
 end
 
-cartfile_updated = !git.modified_files.grep(/Cartfile$/).empty?
+// gemfile_updated = !git.modified_files.grep(/Gemfile$/).empty?
 
-# Leave warning, if Cartfile changes
-if cartfile_updated
-  warn "The `Cartfile` was updated"
-end
+// # Leave warning, if Gemfile changes
+// if gemfile_updated
+//   warn "The `Gemfile` was updated"
+// end
 
-gemfile_updated = !git.modified_files.grep(/Gemfile$/).empty?
-
-# Leave warning, if Gemfile changes
-if gemfile_updated
-  warn "The `Gemfile` was updated"
-end
-
-# This is swiftlint plugin. More info: https://github.com/ashfurrow/danger-swiftlint
-#
-# This lints all Swift files and leave comments in PR if 
-# there is any issue with linting
-swiftlint.lint_files inline_mode: true
-
-# xcodebuild.json_file = "./fastlane/reports/xcpretty-json-formatter-results.json"
-
-# xcodebuild.parse_warnings # returns number of warnings
-# xcodebuild.parse_errors # returns number of errors
-# xcodebuild.parse_tests # returns number of test failures
-# xcodebuild.perfect_build # returns a bool indicating if the build was perfect
+// import xcodebuild
+// xcodebuild.json_file = "./fastlane/reports/xcpretty-json-formatter-results.json"
+// xcodebuild.parse_warnings() // returns number of warnings
+// xcodebuild.parse_errors() // returns number of errors
+// // xcodebuild.parse_errors(errors: danger.github.pull_request.body)
+// xcodebuild.parse_tests()  // returns number of test failures
+// xcodebuild.perfect_build() // returns a bool indicating if the build was perfect
